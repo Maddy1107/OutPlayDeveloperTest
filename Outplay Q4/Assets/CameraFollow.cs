d9ea99ba7a537927a946436f586dbe97537fd7c1
Nilankar Deb <69686900+Maddy1107@users.noreply.github.com>
2021-12-06 21:51:59 +0000

    Initial Commit

---
+++ Outplay Q4/Assets/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 newpos = target.transform.position + offset;
+        transform.position = newpos;
+    }
+}
---
+++ Outplay Q4/Assets/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spawnableObject;
+
+    private void Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            float x = Random.Range(-100, 100);
+            float y = Random.Range(-10, 10);
+            float z = Random.Range(-100, 100);
+            Instantiate(spawnableObject, new Vector3(x, y, z), Quaternion.identity);
+        }
+    }
+}
